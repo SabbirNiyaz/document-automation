3e588631a04ff9049cb788517402c12139c3f28b
SabbirNiyazOfficial <sabbirlm10niyaz@gmail.com>
2026-08-12 10:08:24 +0600

    Fixed dashboard route reference in app header

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -32,7 +32,7 @@ import { UserMenuContent } from '@/components/user-menu-content';
 import { useCurrentUrl } from '@/hooks/use-current-url';
 import { useInitials } from '@/hooks/use-initials';
 import { cn, toUrl } from '@/lib/utils';
-import { dashboard } from '@/routes';
+import { index as documentsIndex } from '@/routes/documents';
 import type { BreadcrumbItem, NavItem } from '@/types';
 
 type Props = {
@@ -42,7 +42,7 @@ type Props = {
 const mainNavItems: NavItem[] = [
     {
         title: 'Dashboard',
-        href: dashboard(),
+        href: documentsIndex(),
         icon: LayoutGrid,
     },
 ];
@@ -135,7 +135,7 @@ export function AppHeader({ breadcrumbs = [] }: Props) {
                     </div>
 
                     <Link
-                        href={dashboard()}
+                        href={documentsIndex()}
                         prefetch
                         className="flex items-center space-x-2"
                     >
